ec0f8a0ecc3912bd24033bf9c7f09f28f4844eb7
Peter <dayr88520@gmail.com>
2024-01-21 09:35:28 +0800

    修改

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBars from "./NavBar/NavBars";
+import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
   //選取的判斷
@@ -7,20 +8,72 @@ const App = () => {
   //視窗大小的判斷 <= 768
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+  useEffect(() => {
+    const handleBlur = () => {
+      if (!isMobile) {
+        document.title = "Thank You For Visiting! - 李俊鋼";
+      }
+    };
+
+    const handleFocus = () => {
+      if (!isMobile) {
+        document.title = "李俊鋼";
+      }
+    };
+
+    window.addEventListener("blur", handleBlur);
+    window.addEventListener("focus", handleFocus);
+
+    return () => {
+      window.removeEventListener("blur", handleBlur);
+      window.removeEventListener("focus", handleFocus);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [selected]);
+
   const renderSelectedComponent = () => {
     switch (selected) {
-        case 0:
-            return <div>Home</div>;
-        case 1:
-            return <div>About</div>;
-        default:
-            return null;
+      case 0:
+        return <div>Home</div>;
+      case 1:
+        return <div>About</div>;
+      default:
+        return null;
     }
-  }
+  };
 
   return (
     <div className="text-slate-100 flex">
-        <NavBars />
+      {isMobile && (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={30}
+          innerScale={1}
+          outerScale={1.5}
+          outerAlpha={0}
+          trailingSpeed={7}
+          innerStyle={{
+            backgroundColor: "rgb(79, 70, 229)",
+          }}
+          outerStyle={{
+            border: "3px solid rgb(79, 70, 229)",
+          }}
+        />
+      )}
+      {renderSelectedComponent()}
+      <NavBars selected={selected} setSelected={setSelected} />
     </div>
   );
 };
--- src/components/NavBar/NavBars.jsx
+++ src/components/NavBar/NavBars.jsx
@@ -2,16 +2,26 @@ import { HiHome } from "react-icons/hi";
 import { IoPerson } from "react-icons/io5";
 import NavItem from "./NavItem";
 
-const NavBars = () => {
+const NavBars = ({ selected, setSelected }) => {
   const navSize = 17;
   return (
-    <nav>
-        {/* Home Page */}
-      <NavItem title="Home">
+    <nav className="h-full w-fit bg-slate-950 p-[0.8rem] ">
+      {/* Home Page */}
+      <NavItem
+        title="Home Page"
+        selected={selected === 0}
+        id={0}
+        setSelected={setSelected}
+      >
         <HiHome size={navSize} />
       </NavItem>
       {/* About Page */}
-      <NavItem>
+      <NavItem
+        title="About Me"
+        selected={selected === 1}
+        id={1}
+        setSelected={setSelected}
+      >
         <IoPerson size={navSize} />
       </NavItem>
     </nav>
